c053dc581552746d9e1611a2947308ee255db2be
Alex Hansen <Hansen.Alexander.J@Gmail.com>
2019-10-03 10:16:16 -0400

    updated settings

--- project_4/settings.py
+++ project_4/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'issuetrackerApp',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +129,11 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ]
 }
+
+REACT_APP_DIR = "client"
+
+STATICFILES_DIRS = [
+  os.path.join(REACT_APP_DIR, 'build', 'static')
+]
+
+django_heroku.settings(locals())
